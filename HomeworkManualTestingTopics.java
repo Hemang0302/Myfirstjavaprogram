184afdf023b8bf4f7482abdff943c23b7c770cd1
Hemang Patel <patel_hemang_r@yahoo.com>
2022-04-02 12:02:18 +0100

    this is my java program

---
+++ HomeworkManualTestingTopics.java
@@ -0,0 +1,40 @@
+package com.learning.java;
+
+import javax.sound.midi.Soundbank;
+
+public class HomeworkManualTestingTopics {
+    public static void main (String[] args) {
+        System.out.println ("Manual Testing");
+        System.out.println ("What is SDLC ?");
+        System.out.println ("Whats is STLC ?");
+        System.out.println ("7 Principles of Software Testing");
+        System.out.println ("Disadvantages of Manual testing");
+        System.out.println ("Different Terms of Manual Testing");
+        System.out.print (" (1) Test Plan ,");
+        System.out.print (" (2) Test Scenario ,");
+        System.out.println (" (3) Test Case .");
+        System.out.print ("Types of testing Techniques");
+        System.out.print (" - ECP(Equivalence class Partitioning ) ,");
+        System.out.print("BVA(Boundary value analysis ) , ");
+        System.out.println("Error Guessing .");
+        System.out.println("Testing Levels");
+        System.out.print("Unit Test , ");
+        System.out.print("Integration Test ,");
+        System.out.print("System Test ,");
+        System.out.println("Acceptance Test .");
+        System.out.println("What is Bug and Bug Template ?");
+        System.out.println("What is White box Testing ?"  );
+        System.out.println("What is Black Box Testing ?");
+        System.out.println("Which are Functional Testing ?" );
+        System.out.print("1.Unit Testing");
+        System.out.print(" 2.Integration testing");
+        System.out.print(" 3.User Acceptance testing");
+        System.out.print(" 4.Smoke Testing");
+        System.out.println(" 5.Sanity Testing");
+        System.out.println("Different types of Software Development Models");
+        System.out.println(" * Water Fall Model, ");
+        System.out.println(" * Iterative Model ,");
+        System.out.println("* V Model ,");
+        System.out.println("* Agile Model ");
+    }
+}
---
+++ testhomework.java
@@ -0,0 +1,8 @@
+package com.learning.java;
+
+public class testhomework {
+    public static void main (String[] args) {
+        System.out.println("Manual Testing");
+        System.out.println();
+    }
+}

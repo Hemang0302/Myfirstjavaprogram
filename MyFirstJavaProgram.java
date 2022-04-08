761cedfb61558928df8f0674e5d91744a8042dcb
hebhai <hemang0302@gmail.com>
2022-04-08 16:58:31 +0100

    change

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -3,6 +3,7 @@ package com.learning.java;
 public class MyFirstJavaProgram {
     public static void main(String[] args) {
 System.out.println("Hemang Patel");
+        System.out.println("Git Homework");
 
     }
 }
